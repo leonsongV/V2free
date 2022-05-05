c403551d443753f5a151149fbaeb2b5de3bc62de
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-05-05 20:37:17 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -42,8 +42,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     helper = CheckIn(args.username, args.password)
     helper.check_in()
-    TOKEN = os.environ.get('PUSH_PLUS_TOKEN') or config.PUSH_PLUS_TOKEN
+    parser.add_argument('PUSH_PLUS_TOKEN')
     if TOKEN:
         print('检测到PUSH_PLUS_TOKEN, 准备推送')
-        title = '什么值得买每日签到' + ('成功' if res.get('error_code') == 0 else '失败')
+        title = 'V2free' + ('成功' if res.get('error_code') == 0 else '失败')
         pushplus(title=title, content=res, token=TOKEN)
